c4c3419db6cbc766bb3926d5b5a13b4b267c0b69
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2023-10-31 13:07:17 -0600

    Add Responsive Navbar for Mobile

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add Responsive Navbar for Mobile Devices
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
